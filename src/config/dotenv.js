79969426ba6cb8624da199c1febebc83148de65d
Michael Castro Montaño <86693441+michaelcm06@users.noreply.github.com>
2023-11-07 16:03:11 -0500

    Delete src/config/dotenv.js

--- src/config/dotenv.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config({path:"./.env", override:true})
-
-export const config={
-    PORT:process.env.PORT||8080,
-    MONGO_URL:process.env.MONGO_URL,
-    DB_NAME:process.env.DB_NAME
-}
